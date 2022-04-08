2b3ccca6ea43e225de416bab3f61810f36d78143
Franciely Lavine <francielylavine@hotmail.com>
2022-04-08 17:42:01 -0300

    com mudanças

--- doc/Front/src/paginas/Login/index.js
+++ doc/Front/src/paginas/Login/index.js
@@ -6,7 +6,14 @@ function Login(){
         <div className='paginaIncial'>
             <div className='boxTexto'>
                 <h1>Controle suas finanças, sem planilha chata.</h1>
-                <h2></h2>
+                <h2>Organizar as suas finanças nunca foi tão fácil, com o DINDIN, você tem tudo num único lugar e em um clique de distância.</h2>
+                <button>Cadastre-se</button>
+            </div>
+            <div className='boxLogin'>
+                <h2>Login</h2>
+                <form>
+                    <label></label>
+                </form>
             </div>
         </div>
     );
