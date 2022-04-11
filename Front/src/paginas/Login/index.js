3837516929f2c335fe4cdb7bd831cc17b6aba31e
Franciely Lavine <francielylavine@hotmail.com>
2022-04-11 13:01:06 -0300

    modificacoes

--- Front/src/paginas/Login/index.js
+++ Front/src/paginas/Login/index.js
@@ -1,9 +1,15 @@
 import { useState, React } from 'react';
+import logo from '../../assets/logo.svg'
 import './index.css';
 
+
 function Login(){
+
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
     return(
         <div className='paginaIncial'>
+            <img src={logo} />
             <div className='boxTexto'>
                 <h1>Controle suas finanças, sem planilha chata.</h1>
                 <h2>Organizar as suas finanças nunca foi tão fácil, com o DINDIN, você tem tudo num único lugar e em um clique de distância.</h2>
@@ -12,7 +18,21 @@ function Login(){
             <div className='boxLogin'>
                 <h2>Login</h2>
                 <form>
-                    <label></label>
+                    <label>E-mail</label>
+                    <input
+                        type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+
+                    <label>Password</label>
+                    <input
+                        type="password"
+                        value={senha}
+                        onChange={(e) => setSenha(e.target.value)}
+                    />
+
+                    <button type="submit">Entrar</button>
                 </form>
             </div>
         </div>
