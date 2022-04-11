a3b3b4adb4ab8467e06cf5266bbc99c6b35ca227
tanurematheus <tanure.matheus@gmail.com>
2022-04-11 16:04:04 -0300

    alterar usuario

--- back/src/rotas.js
+++ back/src/rotas.js
@@ -1,12 +1,11 @@
 const express = require('express');
-const { cadastrarUsuario } = require('./controller/cadastro');
-const detalharUsuario = require('./controller/detalhar_usuario');
-const { login } = require('./controller/login');
+const { cadastrarUsuario, detalharUsuario, login, atualizarUsuario } = require('./controller/usuario');
 const { validateBody, validateEmail, validateEmailLogin, validateLogin } = require('./middleware/middleware');
 
 const rotas = express();
 
 rotas.get('/usuario', validateLogin, detalharUsuario);
+rotas.put('/usuario', validateBody, validateLogin, validateEmail, atualizarUsuario);
 
 rotas.post('/usuario', validateBody, validateEmail, cadastrarUsuario);
 rotas.post('/login', validateBody, validateEmailLogin, login);
