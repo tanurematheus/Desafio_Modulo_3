983894b8cbd97791529210860cfa8895a02c483d
tanurematheus <tanure.matheus@gmail.com>
2022-04-12 13:14:05 -0300

    listar transacoes

--- back/src/routes/rotas_transacao.js
+++ back/src/routes/rotas_transacao.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const { cadastrarTransacao, detalharTransacao } = require('../controller/transacao');
+const { cadastrarTransacao, detalharTransacao, listarTransacoes } = require('../controller/transacao');
 const { validateLogin, validateBody, validateCategoria } = require('../middleware/middleware');
 
 const rotas_transacao = express();
 
 rotas_transacao.post('/transacao', validateLogin, validateBody, validateCategoria, cadastrarTransacao);
+
+rotas_transacao.get('/transacao', validateLogin, listarTransacoes);
 rotas_transacao.get('/transacao/:id', validateLogin, detalharTransacao);
 
 module.exports = rotas_transacao;
